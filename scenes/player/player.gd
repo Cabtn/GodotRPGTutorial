6896270b957f63a97fba1fde97329cea1605a6ef
Abtin Ravanipour <abtinr1185@gmail.com>
2023-12-17 16:54:25 -0800

    First commit for RPG tutorial

---
+++ scenes/player/player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody2D
+
+@export var speed: int = 35
+@onready var animations = $AnimationPlayer
+
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+	
+		animations.play("walk" + direction)
+
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		print_debug(collider.name)
+		
+
+
+func _physics_process(_delta):
+	handleInput()
+	move_and_slide()
+	handleCollision()
+	updateAnimation()
+
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "Hitbox":
+		print_debug(area.get_parent().name)
